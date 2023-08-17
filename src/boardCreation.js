99a4585e02719eb5e980d4f55e75a491839afcc0
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-08-16 19:28:46 -0500

    refactor boardCreation code to make it modular and modify styling of tiles

--- src/boardCreation.js
+++ src/boardCreation.js
@@ -2,63 +2,28 @@ const boardCreation = () => {
     
     const board = document.createElement("div");
     board.classList.add("gameBoard");
+    const colorA = "white";
+    const colorB = "black"
 
-    for (let i = 0; i < 8; i++) {
-        let boardTile = document.createElement("div");
-        boardTile.setAttribute("id", i);
-        (i % 2 == 0) ? boardTile.classList.add("blackTile") : boardTile.classList.add("whiteTile");
-        board.appendChild(boardTile); 
+    const createTiles = (board, columnNumber, firstColor, secondColor ) => {
+        for (let i = 0; i < 8; i++) {
+            let boardTile = document.createElement("div");
+            boardTile.classList.add("tile");
+            boardTile.setAttribute("id", `[${i},${columnNumber}]`);
+            (i % 2 == 0) ? boardTile.classList.add(`${secondColor}`) : boardTile.classList.add(`${firstColor}`);
+            board.appendChild(boardTile); 
+        }
     }
-    
-    for (let i = 8; i < 16; i++) {
-        let boardTile = document.createElement("div");
-        boardTile.setAttribute("id", i);
-        (i % 2 == 0) ? boardTile.classList.add("whiteTile") : boardTile.classList.add("blackTile");
-        board.appendChild(boardTile); 
-    }
-    
-    for (let i = 16; i < 24; i++) {
-        let boardTile = document.createElement("div");
-        boardTile.setAttribute("id", i);
-        (i % 2 == 0) ? boardTile.classList.add("blackTile") : boardTile.classList.add("whiteTile");
-        board.appendChild(boardTile); 
-    }
-    
-    for (let i = 24; i < 32; i++) {
-        let boardTile = document.createElement("div");
-        boardTile.setAttribute("id", i);
-        (i % 2 == 0) ? boardTile.classList.add("whiteTile") : boardTile.classList.add("blackTile");
-        board.appendChild(boardTile); 
-    }
-    
-    for (let i = 32; i < 40; i++) {
-        let boardTile = document.createElement("div");
-        boardTile.setAttribute("id", i);
-        (i % 2 == 0) ? boardTile.classList.add("blackTile") : boardTile.classList.add("whiteTile");
-        board.appendChild(boardTile); 
-    }
-    
-    for (let i = 40; i < 48; i++) {
-        let boardTile = document.createElement("div");
-        boardTile.setAttribute("id", i);
-        (i % 2 == 0) ? boardTile.classList.add("whiteTile") : boardTile.classList.add("blackTile");
-        board.appendChild(boardTile); 
-    }
-    
-    for (let i = 48; i < 56; i++) {
-        let boardTile = document.createElement("div");
-        boardTile.setAttribute("id", i);
-        (i % 2 == 0) ? boardTile.classList.add("blackTile") : boardTile.classList.add("whiteTile");
-        board.appendChild(boardTile); 
-    }
-    
-    for (let i = 56; i < 64; i++) {
-        let boardTile = document.createElement("div");
-        boardTile.setAttribute("id", i);
-        (i % 2 == 0) ? boardTile.classList.add("whiteTile") : boardTile.classList.add("blackTile");
-        board.appendChild(boardTile); 
-    }
-    
+
+    createTiles(board, 0, colorB, colorA);
+    createTiles(board, 1, colorA, colorB);
+    createTiles(board, 2, colorB, colorA);
+    createTiles(board, 3, colorA, colorB);
+    createTiles(board, 4, colorB, colorA);
+    createTiles(board, 5, colorA, colorB);
+    createTiles(board, 6, colorB, colorA);
+    createTiles(board, 7, colorA, colorB);
+
     return board;
 };
 
